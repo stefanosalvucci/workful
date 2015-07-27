775b9b2cb38fbdd4267043fbb2d711e7c2dd2d68
stefanosalvucci <stefano.salvucci@gmail.com>
2015-07-28 01:28:51 +0200

    fix

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,11 @@
 class Item < ActiveRecord::Base
   belongs_to :item_category
+  before_save :validate_amount_save
 
   has_attached_file :image, :styles => { :medium => "400x170>", :thumb => "200x85" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  def validate_amount_save
+    self.amount_save ||= 0
+  end
 end

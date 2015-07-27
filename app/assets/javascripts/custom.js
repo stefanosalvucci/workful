9c3017c603d5f243b95ad542bda6a10982c4fb41
stefanosalvucci <stefano.salvucci@gmail.com>
2015-07-27 12:01:36 +0200

    removed js of arrow

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -226,29 +226,6 @@ $(document).ready(function() {
     /* <!-- =============================================== -->  */
     $(window).scroll(function() {
 
-        var topWindow = $(window).scrollTop();
-        var topWindow = topWindow * 1.5;
-        var windowHeight = $(window).height();
-        var position = topWindow / windowHeight;
-        position = 1 - position;
-        $('.arrow-wrap').css('opacity', position);
-
-        $(function() {
-          $('a[href*=#]:not([href=#])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-              var target = $(this.hash);
-              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-              if (target.length) {
-                $('html,body').animate({
-                  scrollTop: target.offset().top
-                }, 1000);
-                return false;
-              }
-            }
-          });
-        });
-
-
         if ($(this).scrollTop() > 15) {
             $('nav').addClass("navbar-alt")
         } else {

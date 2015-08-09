c9a047391fbfc40328517f9d59d88c9e0189aad6
stefanosalvucci <stefano.salvucci@gmail.com>
2015-08-09 16:07:29 +0200

    changed auth type prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
     domain:               'gmail.com',
     user_name:            'workfuldevelopment',
     password:             'supertester1',
-    authentication:       'plain',
+    authentication:       :login,
     enable_starttls_auto: true
   }
   config.paperclip_defaults = {

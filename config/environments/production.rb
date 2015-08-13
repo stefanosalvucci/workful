c83253575db4456e32281282e11d2a2dd7ddd497
stefanosalvucci <stefano.salvucci@gmail.com>
2015-08-13 14:45:45 +0200

    changed smpt_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,16 +79,19 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'workfulmvp.herokuapp.com' }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'workfuldevelopment',
-    password:             'supertester1',
-    authentication:       :login,
-    enable_starttls_auto: true
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address              => ENV['SMTP_ADDRESS'],
+    :port                 => ENV['SMTP_PORT'],
+    :domain               => ENV['SMTP_DOMAIN'],
+    :user_name            => ENV['SMTP_USERNAME'],
+    :password             => ENV['SMTP_PASSWORD'],
+    :authentication       => :plain,
+    :ssl                       => true,
+    :tls                        => true,
+    :enable_starttls_auto => true
   }
+
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_protocol => :https,

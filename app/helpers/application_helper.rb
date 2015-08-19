add5d57e652b220a0e4862aec0d5a21635e44b81
stefanosalvucci <stefano.salvucci@gmail.com>
2015-08-20 00:17:08 +0200

    customized headline, info_bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,22 @@ module ApplicationHelper
     class_name.join(' ')
   end
 
+  def days_left
+    Time.now.end_of_month.day - Time.now.day
+  end
+
+  def credit_left
+    monthly_budget - credit_used
+  end
+
+  def credit_used
+    ItemSubscription.where(user: current_user).sum(:amount) if current_user
+  end
+
+  def monthly_budget
+    current_user && current_user.monthly_budget
+  end
+
   def display_next_month
     (Date.today + 1.month).strftime("%b %Y")
   end
